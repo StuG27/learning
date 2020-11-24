efad541669b7a93a7172f7795dded81214ef16ae
StuG27 <stug1995@gmail.com>
2020-11-24 23:15:15 +0300

    HT3 Re-done

--- KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/skillbox/ht3/HT3.kt
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/skillbox/ht3/HT3.kt
@@ -5,12 +5,14 @@ import kotlin.math.absoluteValue
 fun main(){
     println("Введите количество чисел")
     val n = readLine()?.toIntOrNull() ?: return
-    var number: Int
+    var number: Int?
     var sum = 0
     var positiveNumbers = 0
-    for (i in 0 until n) {
+    var i = 0
+    while (i < n) {
         println("Введите число")
-        number = readLine()?.toIntOrNull() ?: continue
+        number = readLine()?.toIntOrNull() ?:  continue
+        i++
         sum += number
         if (number > 0) positiveNumbers++
     }
