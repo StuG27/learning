e626f0c893d9699e542df7923fa1cb34310fdf8f
StuG27 <stug1995@gmail.com>
2020-11-25 19:29:19 +0300

    HT4 added

---
+++ KotlinBasic/04_Collections/app/src/main/java/com/skillbox/ht4/HT4.kt
@@ -0,0 +1,31 @@
+package com.skillbox.ht4
+
+fun main() {
+    println("Введите число абонентов")
+    val n = readLine()?.toInt() ?: return
+    var numberList: MutableList<String>
+    numberList = readNumber(n)
+    println("Только номера на +7 ${numberList.filter { it.startsWith("+7") }}")
+    var numberSet = numberList.toSet()
+    println("Уникальных номеров ${numberSet.size}")
+    println("Сумма длин всех номеров ${numberList.sumBy { it.length }}")
+    val numberMap = mutableMapOf<String, String>()
+    for (i in numberSet){
+        println("Введите имя человека с номером телефона $i:")
+        numberMap[i] = readLine().toString()
+    }
+    for ((k, v) in numberMap){
+        println("Человек: $v. Номер телефона: $k")
+    }
+}
+
+fun readNumber(n: Int): MutableList<String> {
+    val numberList = mutableListOf<String>()
+    var number: String
+    for (i in 0 until n){
+        println("Введите номер")
+        number = readLine().toString()
+        numberList.add(number)
+    }
+    return numberList
+}
