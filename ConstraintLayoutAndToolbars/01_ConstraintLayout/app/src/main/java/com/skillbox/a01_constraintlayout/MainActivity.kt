7f50ffb7ad7f20e6b0f27968317df5433b743653
StuG27 <stug1995@gmail.com>
2021-01-09 22:55:18 +0300

    10.8 fix

--- ConstraintLayoutAndToolbars/01_ConstraintLayout/app/src/main/java/com/skillbox/a01_constraintlayout/MainActivity.kt
+++ ConstraintLayoutAndToolbars/01_ConstraintLayout/app/src/main/java/com/skillbox/a01_constraintlayout/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
@@ -12,77 +13,87 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.activity_main.*
+import com.skillbox.a01_constraintlayout.databinding.ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progress: ProgressBar
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var isEmail = false
-        var isPassword = false
-        var isAgree = false
+        progress = ProgressBar(this).apply {
+            layoutParams = ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+            )
+        }
 
         val imageView = findViewById<ImageView>(R.id.iv_head);
         Glide
             .with(this)
             .load("https://i1.sndcdn.com/avatars-000494353437-cuy9dz-t500x500.jpg")
             .into(imageView)
-        b_login.setOnClickListener {
+        binding.bLogin.setOnClickListener {
             login()
         }
 
-        et_email.addTextChangedListener(object : TextWatcher {
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun afterTextChanged(s: Editable?) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                isEmail = s?.let { it.isNotEmpty() } ?: false
-                b_login.isEnabled = isAgree && isEmail && isPassword
+                check()
             }
         })
 
-        et_password.addTextChangedListener(object : TextWatcher {
+        binding.etPassword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun afterTextChanged(s: Editable?) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                isPassword = s?.let { it.isNotEmpty() } ?: false
-                b_login.isEnabled = isAgree && isEmail && isPassword
+                check()
             }
         })
 
-        cb_agree.setOnCheckedChangeListener { _, isChecked ->
-            isAgree = isChecked
-            b_login.isEnabled = isChecked && isEmail && isPassword
+        binding.cbAgree.setOnCheckedChangeListener { _, _ ->
+            check()
         }
+    }
 
-
+    private fun check() {
+        binding.bLogin.isEnabled = binding.cbAgree.isChecked
+                && binding.etEmail.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()
+                && (emailValidate(binding.etEmail.text.toString()))
     }
 
     private fun login() {
-        val progress = ProgressBar(this).apply {
-            layoutParams = ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                ConstraintLayout.LayoutParams.WRAP_CONTENT
-            )
-        }
         progress.id = View.generateViewId()
         val set = ConstraintSet()
-        Cl_main.addView(progress)
-        set.clone(Cl_main)
+        binding.ClMain.addView(progress)
+        set.clone(binding.ClMain)
         set.connect(progress.id, ConstraintSet.TOP, R.id.guideline4, ConstraintSet.BOTTOM,0)
         set.centerHorizontally(progress.id,ConstraintSet.PARENT_ID)
-        set.applyTo(Cl_main)
-        group.referencedIds.forEach {
+        set.applyTo(binding.ClMain)
+
+        binding.group.referencedIds.forEach {
             findViewById<View>(it).isEnabled = false
         }
         Handler().postDelayed({
-            group.referencedIds.forEach {
+            binding.group.referencedIds.forEach {
                 findViewById<View>(it).isEnabled = true
             }
-            Cl_main.removeView(progress)
+            binding.ClMain.removeView(progress)
             Toast.makeText(this, R.string.success, Toast.LENGTH_SHORT).show()
         }, 2000)
+    }
+
+    private fun emailValidate(string: String): Boolean {
+        val myPattern = Patterns.EMAIL_ADDRESS
+        return string.matches(Regex(myPattern.toString()))
     }
 }
 
--- ViewAndLayout/app/src/main/java/com/skillbox/viewandlayout/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/skillbox/viewandlayout/MainActivity.kt
@@ -5,59 +5,24 @@ import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns.EMAIL_ADDRESS
 import android.view.Gravity
+import android.view.View
 import android.widget.*
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.activity_main.*
+import com.skillbox.viewandlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var isEmail = false
-    var isPassword = false
-    var isAgree = false
+    private lateinit var progress: ProgressBar
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val imageView = findViewById<ImageView>(R.id.iv_head);
-        Glide
-                .with(this)
-                .load("https://i1.sndcdn.com/avatars-000494353437-cuy9dz-t500x500.jpg")
-                .into(imageView)
-
-        b_login.setOnClickListener {
-            login()
-        }
-
-        et_email.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun afterTextChanged(s: Editable?) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                isEmail = s?.let{ it.isNotEmpty()}?: false
-                b_login.isEnabled = isAgree && isEmail && isPassword
-            }
-        })
-
-        et_password.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun afterTextChanged(s: Editable?) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                isPassword = s?.let{ it.isNotEmpty()}?: false
-                b_login.isEnabled = isAgree && isEmail && isPassword
-            }
-        })
-
-        cb_agree.setOnCheckedChangeListener { _, isChecked ->
-            isAgree = isChecked
-            b_login.isEnabled = isChecked && isEmail && isPassword
-        }
-
-
-
-    }
-    private fun login() {
-        val progress = ProgressBar(this).apply {
+        progress = ProgressBar(this).apply {
             layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
@@ -65,18 +30,68 @@ class MainActivity : AppCompatActivity() {
                 gravity = Gravity.CENTER_HORIZONTAL
             }
         }
-        b_login.isEnabled = false
-        cb_agree.isEnabled = false
-        et_email.isEnabled = false
-        et_password.isEnabled = false
-        ll_main.addView(progress)
+
+        val imageView = findViewById<ImageView>(R.id.iv_head);
+        Glide
+            .with(this)
+            .load("https://i1.sndcdn.com/avatars-000494353437-cuy9dz-t500x500.jpg")
+            .into(imageView)
+        binding.bLogin.setOnClickListener {
+            login()
+        }
+
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                check()
+            }
+        })
+
+        binding.etPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                check()
+            }
+        })
+
+        binding.cbAgree.setOnCheckedChangeListener { _, _ ->
+            check()
+        }
+    }
+
+    private fun check() {
+        binding.bLogin.isEnabled = binding.cbAgree.isChecked
+                && binding.etEmail.text.isNotEmpty() && binding.etPassword.text.isNotEmpty()
+                && (emailValidate(binding.etEmail.text.toString()))
+    }
+
+    private fun login() {
+
+        var child: View
+        var i = 2
+        while (i < binding.llMain.childCount) {
+            child = binding.llMain.getChildAt(i)
+            child.isEnabled = false
+            i++
+        }
+
+        binding.llMain.addView(progress)
+
         Handler().postDelayed({
-            b_login.isEnabled = true
-            cb_agree.isEnabled = true
-            et_email.isEnabled = true
-            et_password.isEnabled = true
-            ll_main.removeView(progress)
+            while (i > 1) {
+                child = binding.llMain.getChildAt(i)
+                child.isEnabled = true
+                i--
+            }
+            binding.llMain.removeView(progress)
             Toast.makeText(this, R.string.success, Toast.LENGTH_SHORT).show()
         }, 2000)
+    }
+
+    private fun emailValidate(string: String): Boolean {
+        val myPattern = EMAIL_ADDRESS
+        return string.matches(Regex(myPattern.toString()))
     }
 }
