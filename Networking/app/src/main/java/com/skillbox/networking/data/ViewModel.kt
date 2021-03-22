f66edca2e4e8eb31049731477f03af6212ef6200
StuG27 <stug1995@gmail.com>
2021-03-22 20:35:17 +0300

    21.5 fix and reformat

--- Networking/app/src/main/java/com/skillbox/networking/data/ViewModel.kt
+++ Networking/app/src/main/java/com/skillbox/networking/data/ViewModel.kt
@@ -3,7 +3,6 @@ package com.skillbox.networking.data
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import okhttp3.Call
 
 
 class ViewModel : ViewModel() {
